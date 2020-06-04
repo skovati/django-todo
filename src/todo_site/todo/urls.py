65906f654a071e894e4f8ea1b111edcad9a5d328
skovati <mail@skovati.com>
2020-06-04 13:35:58 -0500

    Add creation and deletion mechanism, fixed grid spacing

--- src/todo_site/todo/urls.py
+++ src/todo_site/todo/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import ItemListView
+from . import views
 
 urlpatterns = [
     path('', ItemListView.as_view(), name='todo-home'),
+    path('save_item', views.save_item),
+    path('delete_item/<int:item_id>', views.delete_item),
 ]
--- src/todo_site/todo/views.py
+++ src/todo_site/todo/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.views.generic import ListView
 from .models import Item
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
 
 def index(request):
+    all_items = Item.objects.order_by('-pub_date')
     context = {
-        "items": Item.objects.order_by('-pub_date')
+        'items': all_items
     }
-    return render(request, 'todo/base.html', 'context')
+    return render(request, 'todo/base.html', context)
 
 class ItemListView(ListView):
     model = Item
     template_name = "todo/list.html"
     context_object_name = "items"
     ordering = ["-date"]
+
+@csrf_exempt
+def save_item(request):
+    current_date = timezone.now()
+    submitted_content = request.POST["content"]
+    item = Item()
+    item.content = submitted_content
+    item.date = current_date
+    item.save()
+    return HttpResponseRedirect("/")
+
+@csrf_exempt
+def delete_item(request, item_id):
+    Item.objects.get(id=item_id).delete()
+    return HttpResponseRedirect("/")
