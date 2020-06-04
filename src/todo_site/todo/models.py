93e9c1a1989bdcfb3e287da448ce725e62922100
skovati <mail@skovati.com>
2020-06-04 14:37:11 -0500

    Reduce char limit to 50, add requirements.txt

--- src/todo_site/todo/models.py
+++ src/todo_site/todo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class Item(models.Model):
-    content = models.CharField(max_length=200)
+    content = models.CharField(max_length=50)
     date = models.DateTimeField(default=timezone.now)
     def __str__(self):
         return self.content
